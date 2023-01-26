64688428333c784c71a2f638d9a76dad1d1e3f01
Mike Doggrell <michdoggr@gmail.com>
2023-01-26 13:30:20 -0500

    adding README

--- assets/script.js
+++ assets/script.js
@@ -36,7 +36,7 @@ function listTopics() {
         }
     }
 
- console.log('Here are the topics we learned through Prework:');
+console.log('Here are the topics we learned through Prework:');
 listTopics();
 
 console.log('Which topic should we study first?');
